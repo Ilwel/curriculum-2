29adb558e4a6fedebb5d03818bca8da782f7b5a7
ilwel <ilwelisaac@hotmail.com>
2021-06-19 07:59:57 -0300

    ExperienceCard component

---
+++ src/components/ExperienceCard/index.js
@@ -0,0 +1,16 @@
+import './styles.css'
+
+function ExperienceCard({name}){
+
+    return(
+
+        <div className="experience-card">
+
+            <h1>{name}</h1>
+
+        </div>
+    )
+
+}
+
+export default ExperienceCard;
