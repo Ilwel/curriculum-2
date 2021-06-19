b2a308c86013a9328e3781b949e2b31aa0f2cd58
ilwel <ilwelisaac@hotmail.com>
2021-06-19 07:54:10 -0300

    data files

---
+++ src/message.js
@@ -0,0 +1,36 @@
+
+const message = [
+
+    "Meu histórico com a carreira de programação se iniciou em 2017 quando eu comecei a estudar Ciências da Computação na UECE. Esse primeiro contato foi essencial para eu desenvolver bem os conceitos básicos e boas práticas de programação.",
+ 
+    "Na UECE eu tive contato com minha primeira linguagem de programação: C. Com ela, fui capaz de entender programação seguindo a sintaxe robusta que a linguagem oferecia.",
+ 
+    "Além de C, estudei Python e C++, linguagens onde eu estudei os conceitos básicos do paradigma Orientado a Objeto. Esse paradigma foi um dos motivos da minha paixão por programação.",
+ 
+    "Amo programação pelo mesmo motivo que amo música, desenho, ou design: São processos criativos que dão vazão a minha vontade de mudar o mundo ou ao meu ponto de vista sobre o mesmo.",
+ 
+    "E eu sou apaixonado por me expressar. Por esse motivo amo criar soluções tecnológicas, assim como amo criar músicas ou criar desenhos. A verdade é que aplicar meus conhecimentos para melhorar ou criar coisas no mundo é o cerne de toda minha motivação.",
+ 
+    "Ainda na UECE, eu fui bolsista de uma das equipes da maratona de programação. Esse período foi de suma importância para eu desenvolver uma boa intuição lógica para criar algoritmos.",
+ 
+    "No final de 2018 eu tranquei o curso de Ciências da Computação por alguns motivos pessoais, entre eles a necessidade de eu ter uma renda para desenvolver meus próprios projetos. Então eu passei a procurar um emprego de meio período para ser minha primeira experiência no mercado de tabalho.",
+ 
+    "Em 2019 eu comecei a trabalhar como aprendiz por meio período no Cartório Facundo, 2° Ofício - Eusébio. Junto do curso de Auxiliar Administrativo no IEP, esse trabalho me deu muita experiência sobre como a dinâmica de um escritório funciona. Sou muito grato por tudo o que eu aprendi nesse período.",
+ 
+    "Em paralelo a isso, eu produzi meu álbum musical chamado 'Quarto'. Como eu tinha dito antes, amo produzir músicas. Ter concretizado esse projeto foi algo muito significante pra mim. Ver minhas músicas, os frutos do meu estudo de produção, disponíveis no Spotify me trouxe uma sensação incrível.",
+
+    "Em 2020 foi quando eu terminei meu período no Cartório e a produção do meu álbum. Saindo de lá com meu computador que eu comprei com os salários de aprendiz, eu voltei minha atenção para voltar a estudar uma das minhas paixões: Programação.",
+
+    "Sendo assim, eu passei a ver as tendências de mercado e as linguagens mais populares para desenvolvimento web, e Javasciprit foi a que mais me chamou atenção. Sendo uma ferramenta super difundida tanto no Frontend quanto no Backend.",
+
+    "Por fim eu passei a estudar por conta própria a linguagem e suas ferramentas. Mas, por mais que eu tenha aprendido muita coisa, eu senti falta de uma orientação apropriada de como organizar os assuntos. E é nesse momento que a Cubos Academy entra na minha vida.",
+
+    "Em 2021 me inscrevi na Cubos Academy para criar a base que me desse a segurança para atuar como desenvolvedor Junior. E não me arrependo, a experiência de aprendizado desse curso tem sido fantástica e facilitado muito minha integração com as ferramentas para desenvolvimento web.",
+
+    "Hoje eu tenho como meta me tornar um programador Junior e continuar aprimorando meu conhecimento. Pretendo voltar a fazer faculdade, sendo agora o curso de Sistemas e Mídias, que é o curso de tecnologia voltado para o desenvolvimento de mídias.",
+
+    "E esse foi o breve resumo sobre mim, muito obrigado por ler até aqui, e fique a vontade para navegar nas outras abas deste site."
+
+]
+
+export default message;
---
+++ src/projects.js
@@ -0,0 +1,46 @@
+const projects = [
+
+    {
+
+        name:'Meu Curriculum Vitae',
+        about:'Essa página frontend feita em React é um dos meus primeiros projetos web, nela eu aplico um pouco os conceitos de single page application que o React da suporte',
+        tags:[
+            
+            {text:'React', type:'react'},
+            {text:'Javascript', type:'js'},
+            {text:'HTML', type:'html'},
+            {text:'CSS', type:'css'},
+        ],
+        link:'https://github.com/Ilwel/curriculum',
+
+    },
+    {
+
+        name:'Lista Encadeada em C',
+        about:'Criei o desafio de implementar essa clássica estrutura de dados seguindo a forte sintaxe dessa linguagem',
+        tags:[
+            
+            {text:'C', type:'css'},
+            {text:'Structured Data', type:'node'},
+        ],
+        link:'https://github.com/Ilwel/listaEncadeadaC',
+
+    },
+    {
+
+        name:'SMC CRUD',
+        about:'Fiz um simples CRUD em Express para gerenciar fichas de rpg armazenadas em um banco de dados mySql',
+        tags:[
+            
+            {text:'NodeJS', type:'node'},
+            {text:'Javascript', type:'js'},
+            {text:'mySQL', type:'css'},
+            {text:'Bootstrap', type:'bt'},
+        ],
+        link:'https://github.com/Ilwel/smc_crud',
+
+    },
+
+]
+
+export default projects;
